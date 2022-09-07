d55d2b13286226bba0bd781f4e52518610ada75f
Pranaleerane <112418472+Pranaleerane@users.noreply.github.com>
2022-09-06 23:21:10 -0400

    Delete interface.py

--- interface.py
+++
@@ -1,9 +0,0 @@
-def interface():
-    print("My Program")
-    print("Options:")
-    print("9 - Quit")
-    choice = input("Enter your choice: ")
-    if choice=='9':
-        return
-   
-interface()
