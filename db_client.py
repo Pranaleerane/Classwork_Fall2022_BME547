deed66bb23b1fc9abeae571276238cbbb1b85cdc
Pranali Rane <pranali.rane@duke.edu>
2022-10-27 20:00:39 -0400

    server practice class work

--- db_client.py
+++ db_client.py
@@ -1,5 +1,15 @@
+#%%
 import requests
 
 
-out_data("ann ables", 1, "A+")
-r = requests.post("http://127.0.0.1:5000/new_patient", json.out_data)
+out_data = {"name": "ann ables","id": 1,"blood_type": "A+"}
+r = requests.post("http://127.0.0.1:5000/new_patient", json =out_data)
+print(r.status_code)
+print(r.text)
+
+
+test_data = {"id": 1, "test_name": "HDL","test_result": 60}
+r = requests.post("http://127.0.0.1:5000/add_test", json = test_data)
+print(r.status_code)
+print(r.text)
+#%%
--- dp_server.py
+++ dp_server.py
@@ -77,6 +77,44 @@ def validate_new_patient_info(in_data):
     return True
 
 
+@app.route("/add_test", methods = ["POST"])
+def add_test_to_server(in_data):
+    in_data = request.get_json()
+    message, status_code = add_test_worker(in_data)
+    return message, status_code
+
+
+def find_patient(in_data):
+    for patient in db:
+        if patient["id"] == in_data:
+            return patient 
+    return False
+
+def add_test_worker(in_data):
+    msg = add_test_validation(in_data)
+    if msg is not True:
+        return msg, 400
+    add_test_to_patient(in_data)
+    return "Test added", 200
+
+
+def add_test_to_patient(in_data):
+    patient = find_patient(in_data["id"])
+    patient["test_name"].append(in_data["test_name"])
+    patient["test_result"].append(in_data["test_result"])
+
+def add_test_validation(in_data):
+    if type(in_data) is not dict:
+        return "POST data was not dictionary"
+    expected_keys = ["id", "test_name", "test_result"]
+    for key in expected_keys:
+        if key not in in_data:
+            return ("Key {} is missing from POST data".format(key))
+    expected_types = [int, str, int]
+    for key, ex_type in zip(expected_keys, expected_types):
+        if type(in_data[key]) is not ex_type:
+            return ("Key {}'s value has the wrong data type".format(key))
+    return True
 
 if __name__ == '__main__':
     init_server()
