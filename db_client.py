769e39859276d9e0690bcc73ec767e493e4a9cb0
Pranali Rane <pranali.rane@duke.edu>
2022-11-16 12:21:11 -0500

    GUI designing intro lecture

--- db_client.py
+++ db_client.py
@@ -1,11 +1,11 @@
 #%%
 import requests
 
+def upload_patient_info(patient_name, patient_id, patient_blood_type):
 
-out_data = {"name": "ann ables","id": 1,"blood_type": "A+"}
-r = requests.post("http://127.0.0.1:5000/new_patient", json =out_data)
-print(r.status_code)
-print(r.text)
+    out_data = {"name": "ann ables","id": 1,"blood_type": "A+"}
+    r = requests.post("http://127.0.0.1:5000/new_patient", json =out_data)
+    return r.text, r.status_code
 
 
 test_data = {"id": 1, "test_name": "HDL","test_result": 60}
